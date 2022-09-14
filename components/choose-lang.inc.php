a29d20fa7b5eca28861df997ff48348923cd7a5a
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-09-14 17:02:32 +0530

    Add language choosing section

---
+++ components/choose-lang.inc.php
@@ -0,0 +1,21 @@
+<!--========================= Choose Language Section========================= -->
+<section id="about" class="about">
+<div class="container">
+  <div class="row">
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="../assets/img/choose.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Choose your language</h5>
+    <p class="card-text">You may continue with your convinient language</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">English</li>
+    <li class="list-group-item">Sinhala</li>
+    <li class="list-group-item">Tamil</li>
+  </ul>
+</div>
+</div>
+</div>
+</section>
+
+<!--========================= End Choose Language Section========================= -->
--- pages/index.php
+++ pages/index.php
@@ -1,2 +1,4 @@
 <?php include('../components/header.inc.php'); ?>
+<?php include('../components/choose-lang.inc.php'); ?>
+
 <?php include('../components/footer.inc.php'); ?>
