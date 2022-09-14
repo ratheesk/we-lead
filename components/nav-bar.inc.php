a6eb6452cfba9718c34df0f8790b79d0db223c4b
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-09-14 16:50:35 +0530

    Fix footer at bottom

--- components/nav-bar.inc.php
+++ components/nav-bar.inc.php
@@ -5,12 +5,7 @@
     <img src="../assets/img/logo/University_of_Peradeniya_crest.png" width="40" height="40" class="d-inline-block align-top" alt="">
     Career Guidance Unit
   </a>
-<!-- start nav bar -->
-    <nav class="nav-menu d-none d-lg-block">
-      <ul>
-        <li class="active"><a href="../pages/index.php">Home</a></li>
-      </ul>
-    </nav><!-- End navbar -->
+<!-- start nav bar --><!-- End navbar -->
 
     <a href="https://www.pdn.ac.lk/cgu/" class="get-started-btn scrollto">Find more</a>
 
