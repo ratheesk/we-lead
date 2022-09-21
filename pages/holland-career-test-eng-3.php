4bf4094f2119792f296df9d81422f9b19b02eba0
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-09-21 16:32:10 +0530

    Create dummy form for page 3

--- pages/holland-career-test-eng-3.php
+++ pages/holland-career-test-eng-3.php
@@ -4,14 +4,20 @@
 <?php
 session_start();
 // Checking first page values for empty,If it finds any blank field then redirected to first page.
-if (isset($_POST['name'])){
- if (empty($_POST['name'])){ 
+if (isset($_POST['gender'])){
+ if (empty($_POST['gender'])){ 
  // Setting error message
- $_SESSION['error'] = "Mandatory field(s) are missing, Please fill it again";
- header("location: holland-career-test-eng-1.php"); // Redirecting to first page 
- } 
+ $_SESSION['error_page2'] = "Mandatory field(s) are missing, Please fill it again";
+ header("location: holland-career-test-eng-2.php"); // Redirecting to first page 
+ }
+ else {
+    // Fetching all values posted from second page and storing it in variable.
+    foreach ($_POST as $key => $value) {
+    $_SESSION['post'][$key] = $value;
+    }
+    } 
 } else {
- if (empty($_SESSION['error_page2'])) {
+ if (empty($_SESSION['error_page3'])) {
  header("location: holland-career-test-eng-1.php");//redirecting to first page
  }
 }
@@ -23,18 +29,17 @@ if (isset($_POST['name'])){
  <span id="error">
 <?php
 // To show error of page 2.
-if (!empty($_SESSION['error_page2'])) {
- echo $_SESSION['error_page2'];
- unset($_SESSION['error_page2']);
+if (!empty($_SESSION['error_page3'])) {
+ echo $_SESSION['error_page3'];
+ unset($_SESSION['error_page3']);
 }
 ?>
  </span>
 
- <form action="holland-career-test-eng-3.php" method="post">
+ <form action="holland-career-test-eng-4.php" method="post">
  <div class="form-group">
- <label>Gender :<span>*</span></label>
- <input type="radio" name="gender" value="male" required>Male
- <input type="radio" name="gender" value="female">Female
+ <label>City :<span>*</span></label>
+ <input name="city" id="city" type="text" size="25" required>
 </div>
 <div class="form-group">
  <input type="reset" value="Reset" />
