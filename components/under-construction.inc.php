22e3b0636bfd6ee0b394801731a632c4dda80258
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-09-21 15:09:47 +0530

    Make images as responsive

--- components/under-construction.inc.php
+++ components/under-construction.inc.php
@@ -1,11 +1,10 @@
-<!--========================= Under Construction Section========================= -->
-<section id="under-construction">
-        <div class="row justify-content-center">
-            <div class="card text-black">
+<!--========================= Under Construction ========================= -->
+
+        <div class="row justify-content-center" id="under-construction">
+            <div class="card text-black border-0  text-center">
                 <h2>Page is under construction</h2>
-                <img class="card-img" src="../assets/img/under-construction.png" alt="Page is Under Construction">
+                <img class="img-fluid" src="../assets/img/under-construction.png" alt="Page is Under Construction">
             </div>
         </div>
-</section>
 
-<!--========================= End Under Construction Section========================= -->
+<!--========================= End Under Construction ========================= -->
