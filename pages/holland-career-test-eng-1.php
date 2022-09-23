944f62ef9ad31ba4f1f2d36e4ec6620269dd49a5
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-09-23 15:27:11 +0530

    Develop basic ui for the quiz

--- pages/holland-career-test-eng-1.php
+++ pages/holland-career-test-eng-1.php
@@ -4,40 +4,60 @@
 session_start(); // Session starts here.
 ?>
 
- <!--========================= Choose Language Section========================= -->
+<!--========================= Choose Language Section========================= -->
 <section id="choose-language">
-<div class="container">
-  <div class="row justify-content-center">
-<div class="card" style="max-width: 400px;">
-  <div class="card-body">
-  <span id="error">
- <!---- Initializing Session for errors --->
- <?php
+    <span id="error">
+        <!---- Initializing Session for errors --->
+        <?php
  if (!empty($_SESSION['error'])) {
  echo $_SESSION['error'];
  unset($_SESSION['error']);
  }
  ?>
- </span>
- <form action="holland-career-test-eng-2.php" method="post">
-<div class="form-group">
- <div class="form-check form-check-inline">
-  <input class="form-check-input" type="radio" name="q1" id="inlineRadio1" value="option1">
-  <label class="form-check-label" for="inlineRadio1">1</label>
-</div>
-<div class="form-check form-check-inline">
-  <input class="form-check-input" type="radio" name="q1" id="inlineRadio2" value="option2">
-  <label class="form-check-label" for="inlineRadio2">2</label>
-</div>
-</div>
-<div class="form-group">
- <input type="reset" value="Reset" />
- <input type="submit" value="Next" />
- </div>
- </form>
-</div>
-</div>
-</div>
+    </span>
+    <form action="holland-career-test-eng-2.php" method="post">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6">
+                    <p>Repair a dishwasher</p>
+                </div>
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="q1" id="q1" value="1"  required="required">
+                            <label class="form-check-label" for="q1_1">1</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="q1" id="q1_2" value="2">
+                            <label class="form-check-label" for="q2">2</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <p>Use precision machines to build custom metal parts</p>
+                </div>
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="q2" id="q2_1" value="1" required="required">
+                            <label class="form-check-label" for="q1">1</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="q2" id="q2_2" value="2">
+                            <label class="form-check-label" for="q2">2</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <input type="reset" value="Reset" />
+            <input type="submit" value="Next" />
+        </div>
+    </form>
 </section>
 
 <!--========================= End Choose Language Section========================= -->
