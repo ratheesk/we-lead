8b40e8a5caa950cb4a50bf023cc8926191fadbb8
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-09-26 13:28:31 +0530

    Create the original quiz question 1

--- pages/holland-career-test-eng-1.php
+++ pages/holland-career-test-eng-1.php
@@ -30,64 +30,22 @@ session_start(); // Session starts here.
                         <div class="form-check form-check-inline">
                             <input class="form-check-input" type="radio" name="k_1" id="K_1_2" value="2">
                             <label class="form-check-label" for="K_1_2">2</label>
+                        </div>                        
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="k_1" id="K_1_3" value="3">
+                            <label class="form-check-label" for="K_1_3">3</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="k_1" id="K_1_4" value="4">
+                            <label class="form-check-label" for="K_1_4">4</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="k_1" id="K_1_5" value="5">
+                            <label class="form-check-label" for="K_1_5">5</label>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-md-6">
-                    <p>Repair a dishwasher</p>
-                </div>
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="k_2" id="k_2_1" value="1"  required="required">
-                            <label class="form-check-label" for="k_2_1">1</label>
-                        </div>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="k_2" id="k_2_2" value="2">
-                            <label class="form-check-label" for="k_2_2">2</label>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-6">
-                    <p>Analyze the structure of molecules</p>
-                </div>
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="q_1" id="q_1_1" value="1" required="required">
-                            <label class="form-check-label" for="q_1_1">1</label>
-                        </div>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="q_1" id="q_1_2" value="2">
-                            <label class="form-check-label" for="q_1_2">2</label>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-                <div class="col-md-6">
-                    <p>Analyze the structure of molecules</p>
-                </div>
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="q_2" id="q_2_1" value="1" required="required">
-                            <label class="form-check-label" for="q_2_1">1</label>
-                        </div>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="q_2" id="q_2_2" value="2">
-                            <label class="form-check-label" for="q_2_2">2</label>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
         <div class="form-group">
             <input type="reset" value="Reset" />
             <input type="submit" value="Next" />
