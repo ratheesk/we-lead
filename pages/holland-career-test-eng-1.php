ecca8aef129f09ab889ec6ba13e21f8b7c883171
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-09-26 14:05:51 +0530

    Add question 4

--- pages/holland-career-test-eng-1.php
+++ pages/holland-career-test-eng-1.php
@@ -111,29 +111,29 @@ session_start(); // Session starts here.
             </div>
             <div class="row align-middle border pt-3 ml-2 mr-2">
                 <div class="col-md-6">
-                    <p>Repair a dishwasher</p>
+                    <p>Do scientific experiments</p>
                 </div>
                 <div class="col-md-6">
                     <div class="form-group">
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="k_1" id="k_1_1" value="1"  required="required">
-                            <label class="form-check-label" for="k_1_1">1</label>
+                            <input class="form-check-input" type="radio" name="q_2" id="q_2_1" value="1"  required="required">
+                            <label class="form-check-label" for="q_2_1">1</label>
                         </div>
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="k_1" id="K_1_2" value="2">
-                            <label class="form-check-label" for="K_1_2">2</label>
+                            <input class="form-check-input" type="radio" name="q_2" id="q_2_2" value="2">
+                            <label class="form-check-label" for="q_2_2">2</label>
                         </div>                        
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="k_1" id="K_1_3" value="3">
-                            <label class="form-check-label" for="K_1_3">3</label>
+                            <input class="form-check-input" type="radio" name="q_2" id="q_2_3" value="3">
+                            <label class="form-check-label" for="q_2_3">3</label>
                         </div>
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="k_1" id="K_1_4" value="4">
-                            <label class="form-check-label" for="K_1_4">4</label>
+                            <input class="form-check-input" type="radio" name="q_2" id="q_2_4" value="4">
+                            <label class="form-check-label" for="q_2_4">4</label>
                         </div>
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="k_1" id="K_1_5" value="5">
-                            <label class="form-check-label" for="K_1_5">5</label>
+                            <input class="form-check-input" type="radio" name="q_2" id="q_2_5" value="5">
+                            <label class="form-check-label" for="q_2_5">5</label>
                         </div>
                     </div>
                 </div>
