40414c5dbf713fabf685b56b35da2bebcb02b639
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-09-26 12:51:25 +0530

    Organise the quiz questions

--- reference/ref-questions.php
+++ reference/ref-questions.php
@@ -1,245 +1,321 @@
 <!-- - set 1-->
 
-    Repair a dishwasher 
-                    [r_1]"        
+<a>
+    Repair a dishwasher
+    [r_1]
+</a>
+<a>
 
-    Use precision machines to build custom metal parts 
-                    [r_2]"                
+    Use precision machines to build custom metal parts
+    [r_2]
+</a>
+<a>
 
-    Analyze the structure of molecules        
-                    [i_1]" 
-               
-    Do scientific experiments        
-                    [i_2]" 
+    Analyze the structure of molecules
+    [i_1]
+</a>
+<a>
 
-    Design a magazine cover        
-                    [a_1]" 
+    Do scientific experiments
+    [i_2]
+</a>
+<a>
 
-    Paint a portrait 
-                    [a_2]" 
+    Design a magazine cover
+    [a_1]
+</a>
+<a>
 
-    Teach social skills to disabled
-            children 
-                    [s_1]" 
+    Paint a portrait
+    [a_2]
+</a>
+<a>
 
-    Teach adults to read 
-                    [s_2]" 
+    Teach social skills to disabled children
+    [s_1]
+</a>
+<a>
+
+    Teach adults to read
+    [s_2]
+</a>
+<a>
 
     Coordinate a business conference
-        
-                    [e_1]" 
+    [e_1]
+</a>
+<a>
 
     Plan a marketing strategy for a new
-            company 
-                    [e_2]" 
+    company
+    [e_2]
+</a>
+<a>
 
     Track monthly expenses for a company
-        
-                    [c_1]" 
+    [c_1]
+</a>
+<a>
 
     Review financial records for accuracy
-        
-                    [c_2]" 
+    [c_2]
+</a>
 <!-- - set 2-->
 
 
-    Install a hardwood floor 
-                
-                    [r_3]" 
+<a>
+    Install a hardwood floor
+    [r_3]
+
+</a>
+<a>
 
     Repair an air conditioning system
-             
-                    [r_4]" 
+    [r_4]
+</a>
+<a>
 
-    Research the properties of nuclear
-            energy  
-                    [i_3]" 
+    Research the properties of nuclear energy
+    [i_3]
+</a>
+<a>
 
-    Research a new medicine 
-                
-                    [i_4]" 
+    Research a new medicine
 
-    Design a greeting card 
-                
-                    [a_3]" 
+    [i_4]
+</a>
+<a>
 
-    Illustrate a children’s book 
-                
-                    [a_4]" 
+    Design a greeting card
 
-    Help a disabled person with their
-            daily routine  
-                    [s_3]" 
+    [a_3]
+</a>
+<a>
 
-    Tutor a child with a learning
-            disability  
-                    [s_4]" 
+    Illustrate a children’s book
 
-    Negotiate business partnerships 
-                
-                    [e_3]" 
+    [a_4]
+</a>
+<a>
+
+    Help a disabled person with their daily routine
+    [s_3]
+</a>
+<a>
+
+    Tutor a child with a learning disability
+    [s_4]
+</a>
+<a>
+
+    Negotiate business partnerships
+    [e_3]
+</a>
+<a>
 
     Motivate employees to achieve success
-             
-                    [e_4]" 
+    [e_4]
+</a>
+<a>
 
-    Keep payroll records 
-                
-                    [c_3]" 
+    Keep payroll records
+    [c_3]
+</a>
+<a>
 
-    Use spreadsheets to organize
-            financial data  
-                    [c_4]" 
+    Use spreadsheets to organize financial data
+    [c_4]
+</a>
 
-<!-- - set 3-->
+    <!-- - set 3-->
 
+    <a>
 
     Install an alarm system in a building
-             
-                    [r_5]" 
+    [r_5]
 
-    Install kitchen cabinets 
-                
-                    [r_6]" 
+</a>
+<a>
+
+    Install kitchen cabinets
+    [r_6]
+</a>
+<a>
 
     Look at cells through a microscope
-             
-                    [i_5]" 
+    [i_5]
+</a>
+<a>
 
-    Work in a chemistry lab 
-                
-                    [i_6]" 
+    Work in a chemistry lab
+    [i_6]
+</a>
+<a>
 
-    Design magazine ads 
-                
-                    [a_5]" 
+    Design magazine ads
+    [a_5]
+</a>
+<a>
 
     Write a script for a television show
-             
-                    [a_6]" 
+    [a_6]
+</a>
+<a>
 
-    Counsel a person recovering from drug
-            addiction  
-                    [s_5]" 
+    Counsel a person recovering from drug addiction
+    [s_5]
+</a>
+<a>
 
     Counsel a person with depression
-             
-                    [s_6]" 
+    [s_6]
+</a>
+<a>
 
-    Hire and fire employees 
-                
-                    [e_5]" 
+    Hire and fire employees
+    [e_5]
+</a>
+<a>
 
     Close an important business deal
-             
-                    [e_6]" 
+    [e_6]
+</a>
+<a>
 
-    Check tax returns for errors 
-                
-                    [c_5]" 
+    Check tax returns for errors
+    [c_5]
+</a>
+<a>
 
-    Calculate the cost of an insurance
-            claim  
-                    [c_6]" 
+    Calculate the cost of an insurance  claim
+    [c_6]
+</a>
+<a>
 
-<!-- - set 4-->
+    <!-- - set 4-->
 
 
-    Build a stone wall 
-                
-                    [r_7]" 
+    Build a stone wall
+    [r_7]
 
-    Operate a bulldozer 
-                
-                    [r_8]" 
+</a>
+<a>
+
+    Operate a bulldozer
+    [r_8]
+</a>
+<a>
 
     Analyze soil samples for pollution
-             
-                    [i_7]" 
+    [i_7]
+</a>
+<a>
 
-    Study a fault line to predict
-            earthquakes  
-                    [i_8]" 
+    Study a fault line to predict earthquakes
+    [i_8]
+</a>
+<a>
 
     Design a billboard advertisement
-             
-                    [a_7]" 
+    [a_7]
+</a>
+<a>
 
-    Edit a movie 
-                
-                    [a_8]" 
+    Edit a movie
+    [a_8]
+</a>
+<a>
 
-    Plan educational games for preschool
-            children  
-                    [s_7]" 
+    Plan educational games for preschool children
+    [s_7]
+</a>
+<a>
 
     Plan activities for elderly people
-             
-                    [s_8]" 
+    [s_8]
+</a>
+<a>
 
-    Lead a team 
-                
-                    [e_7]" 
+    Lead a team
+    [e_7]
+</a>
+<a>
 
-    Start a new business 
-                
-                    [e_8]" 
+    Start a new business
+    [e_8]
+</a>
+<a>
 
-    Calculate the cost of a construction
-            project  
-                    [c_7]" 
+    Calculate the cost of a construction project
+    [c_7]
+</a>
+<a>
 
-    Help customers fill out loan
-            applications  
-                    [c_8]" 
+    Help customers fill out loan applications
+    [c_8]
+</a>
+<a>
 
-<!-- - set 5-->
+    <!-- - set 5-->
 
 
-    Take apart a car engine 
-                
-                    [r_9]" 
+    Take apart a car engine
+    [r_9]
 
-    Inspect a roof for leaks 
-                
-                    [r_1]" 
+</a>
+<a>
 
-    Do laboratory tests to diagnose
-            diseases  
-                    [i_9]" 
+    Inspect a roof for leaks
+    [r_1_0]
+</a>
+<a>
 
-    Research heat esistant materials
-            for airplane engines  
-                    [i_1]" 
+    Do laboratory tests to diseases
+    [i_9]
+</a>
+<a>
 
-    Compose a song 
-                
-                    [a_9]" 
+    Research heat esistant materials for airplane engines
+    [i_1_0]
+</a>
+<a>
 
-    Write a poem 
-                
-                    [a_1]" 
+    Compose a song
+    [a_9]
+</a>
+<a>
 
-    Help diabetic patients plan a proper
-            di -- set  
-                    [s_9]" 
+    Write a poem
+    [a_1_0]
+</a>
+<a>
 
-    Help a needy family find appropriate
-            housing  
-                    [s_1]" 
+    Help diabetic patients plan a proper diet plan
+    [s_9]
+</a>
+<a>
+
+    Help a needy family find appropriate housing
+    [s_1_0]
+</a>
+<a>
 
     Give a speech in front of many people
-             
-                    [e_9]" 
+    [e_9]
+</a>
+<a>
 
     Persuade others to my point of view
-             
-                    [e_1]" 
+    [e_1_0]
+</a>
+<a>
 
-    Collect tax payments 
-                
-                    [c_9]" 
+    Collect tax payments
 
-    Develop a budg -- set for a city
-            government  
-                    [c_1]" 
+    [c_9]
+</a>
+<a>
+
+    Develop a budget for a city government
+    [c_1_0]
+</a>
