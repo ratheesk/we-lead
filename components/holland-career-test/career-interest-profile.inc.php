87b4f2652ddc3d79c0b6421da25a6f40d4a3c1dd
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-10-03 16:20:16 +0530

    Write comments

--- components/holland-career-test/career-interest-profile.inc.php
+++ components/holland-career-test/career-interest-profile.inc.php
@@ -1,3 +1,4 @@
+<!--chartjs-->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
 
 <h4 class="card-title">Your Career Interest Profile</h4>
@@ -6,6 +7,7 @@
     will appeal to you, while others will be less attractive. Choosing a career which is a good match
     for your interest profile ensures that you enjoy your daily work and get satisfaction out of your
     accomplishments.</p>
+
 <?php
  //get the percentages
  $building = ($k / 10) * 100;
@@ -16,44 +18,53 @@
  $organising = ($p / 10) * 100;
  echo 'Your primary interest area is '.$primary_interest_area[0].' and is in ' . $primary_interest_area[1];
  ?>
+
+<!--Bar chart-->
 <div class="d-flex justify-content-center">
-<canvas id="myChart" height="400px" style="width:100%; max-width:600px"></canvas>
+    <canvas id="myChart" height="400px" style="width:100%; max-width:600px"></canvas>
 </div>
+<!--End of Bar chart-->
+
+<!--Script for bar chart-->
 <script>
-var xValues = ["Bbuilding", "Thinking", "Creating", "Helping", "Persuading","Organising"];
-var yValues = [<?php echo $building; ?>, 
-                <?php echo $thinking; ?>, 
-                <?php echo $creating; ?>, 
-                <?php echo $helping; ?>, 
-                <?php echo $persuading; ?>, 
-                <?php echo $organising; ?>];
-var barColors = ["red", "green","blue","orange","brown","yellow"];
+var xValues = ["Building", "Thinking", "Creating", "Helping", "Persuading", "Organising"];
+var yValues = [<?php echo $building; ?>,
+    <?php echo $thinking; ?>,
+    <?php echo $creating; ?>,
+    <?php echo $helping; ?>,
+    <?php echo $persuading; ?>,
+    <?php echo $organising; ?>
+];
+var barColors = ["red", "green", "blue", "orange", "brown", "yellow"];
 
 new Chart("myChart", {
-  type: "bar",
-  data: {
-    labels: xValues,
-    datasets: [{
-      backgroundColor: barColors,
-      data: yValues
-    }]
-  },
-  options: {
-    legend: {display: false},
-    title: {
-      display: true,
-      text: "Your interest"
-    },
-    scales: {
-        yAxes: [{
-            display: true,
-            ticks: {
-                beginAtZero: true,
-                steps: 10,   // minimum value will be 0.
-                max: 100
-            }
+    type: "bar",
+    data: {
+        labels: xValues,
+        datasets: [{
+            backgroundColor: barColors,
+            data: yValues
         }]
+    },
+    options: {
+        legend: {
+            display: false
+        },
+        title: {
+            display: true,
+            text: "Your interest areas"
+        },
+        scales: {
+            yAxes: [{
+                display: true,
+                ticks: {
+                    beginAtZero: true, // minimum value will be 0.
+                    steps: 10, 
+                    max: 100
+                }
+            }]
         }
-  }
+    }
 });
-</script>   
+</script>
+<!--End of Script for bar chart-->
