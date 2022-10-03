4715b08421a91c30646110025faed030bf2f47db
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-10-03 14:43:03 +0530

    Write function to get primary interest

--- components/holland-career-test/career-interest-profile.inc.php
+++ components/holland-career-test/career-interest-profile.inc.php
@@ -1,8 +1,30 @@
 <?php
 
     // Echo session variables that were set on previous page
-    echo $_SESSION['thinking'];
-    echo $_SESSION['building'];
+    $building = $_SESSION['building'];
+    $thinking = $_SESSION['thinking'];
+    $creating = $_SESSION['creating'];
+    $helping = $_SESSION['helping'];
+    $persuading = $_SESSION['persuading'];
+    $organising = $_SESSION['organising'];
+    echo $building;
+    echo $thinking;
+
+    function primary_interest($args) {
+        $results = array();
+        $max_value = max($args);
+        $results[] = $max_value;
+     
+        //now get the key for the first occurence of max value
+        $found_key = false;
+        foreach ($args as $key => $value) {
+            if ($value == $max_value && !$found_key) {
+                $results[] =  $key;
+                $found_key = true;
+            }
+        }
+        return $results;
+    }
     ?> 
 <h4 class="card-title">Your Career Interest Profile</h4>
 <p class="card-text">This section shows your top career interest areas. There are 6 total interest
