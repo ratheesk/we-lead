efe4cbe4b7fc005c3a8bba7a8dcd47e22a464a9f
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-10-03 16:34:43 +0530

    Add interest areas explanation

--- components/holland-career-test/career-interest-profile.inc.php
+++ components/holland-career-test/career-interest-profile.inc.php
@@ -31,19 +31,25 @@
     are drawn to each of these interest areas tend to have certain characteristics, preferences, and personality traits
     in common.</p>
 
-    <div class="row align-middle border-0 pt-3 ml-2 mr-2">
+<div class="row align-middle border-0 pt-3 ml-2 mr-2">
     <div class="col-md-2"><strong>Building</strong></div>
-    <div class="col-md-10">Building jobs involve the use of tools, machines, or physical skill. Builders like working with their hands and bodies, working with plants and animals, and working outdoors.</div>
-    <div class="col-md-2 mt-3"><strong>Building</strong></div>
-    <div class="col-md-10 mt-2"></div>  
-    <div class="col-md-2 mt-3"><strong>Building</strong></div>
-    <div class="col-md-10 mt-2"></div>  
-    <div class="col-md-2 mt-3"><strong>Building</strong></div>
-    <div class="col-md-10 mt-2"></div>  
-    <div class="col-md-2 mt-3"><strong>Building</strong></div>
-    <div class="col-md-10 mt-2"></div>  
-    <div class="col-md-2 mt-3"><strong>Building</strong></div>
-    <div class="col-md-10 mt-2"></div>    
+    <div class="col-md-10">Building jobs involve the use of tools, machines, or physical skill. Builders like working
+        with their hands and bodies, working with plants and animals, and working outdoors.</div>
+    <div class="col-md-2 mt-3"><strong>Thinking</strong></div>
+    <div class="col-md-10 mt-2">Thinking jobs involve theory, research, and intellectual inquiry. Thinkers like working
+        with ideas and concepts, and enjoy science, technology, and academia.</div>
+    <div class="col-md-2 mt-3"><strong>Creating</strong></div>
+    <div class="col-md-10 mt-2">Creating jobs involve art, design, language, and self-expression. Creators like working
+        in unstructured environments and producing something unique.</div>
+    <div class="col-md-2 mt-3"><strong>Helping</strong></div>
+    <div class="col-md-10 mt-2">Helping jobs involve assisting, teaching, coaching, and serving other people. Helpers
+        like working in cooperative environments to improve the lives of others.</div>
+    <div class="col-md-2 mt-3"><strong>Persuading</strong></div>
+    <div class="col-md-10 mt-2">Persuading jobs involve leading, motivating, and influencing others. Persuaders like
+        working in positions of power to make decisions and carry out projects.</div>
+    <div class="col-md-2 mt-3"><strong>Organising</strong></div>
+    <div class="col-md-10 mt-2">Organizing jobs involve managing data, information, and processes. Organizers like to
+        work in structured environments to complete tasks with precision and accuracy.</div>
 </div>
 <!--End of Interest areas explanation-->
 
