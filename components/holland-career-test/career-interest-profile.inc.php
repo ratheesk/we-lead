b5d30ddb2e9d93fc96ae9cfb794b1c6b552edd48
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-10-03 14:26:35 +0530

    Add validation for page 5

--- components/holland-career-test/career-interest-profile.inc.php
+++ components/holland-career-test/career-interest-profile.inc.php
@@ -1,6 +1,13 @@
+<?php
+
+    // Echo session variables that were set on previous page
+    echo $_SESSION['thinking'];
+    echo $_SESSION['building'];
+    ?> 
 <h4 class="card-title">Your Career Interest Profile</h4>
 <p class="card-text">This section shows your top career interest areas. There are 6 total interest
     areas, each with its own set of typical work tasks, roles, and values. Some of these interest areas
     will appeal to you, while others will be less attractive. Choosing a career which is a good match
     for your interest profile ensures that you enjoy your daily work and get satisfaction out of your
     accomplishments.</p>
+
--- pages/holland-career-test-eng-6.php
+++ pages/holland-career-test-eng-6.php
@@ -1,27 +1,38 @@
 <?php include('../components/header.inc.php'); ?>
 
+
 <?php
- //session_start();
- //if (isset($_POST['k_9'])) {
- //if (!empty($_SESSION['post'])){
- //if (empty($_POST['k_9'])){ 
- //// Setting error for page 3.
- //$_SESSION['error_page5'] = "Mandatory field(s) are missing, Please fill it again";
- //header("location: holland-career-test-eng-5.php"); // Redirecting to third page.
- //} else {
- //foreach ($_POST as $key => $value) {
- //$_SESSION['post'][$key] = $value;
- //} 
- //extract($_SESSION['post']); // Function to extract array.
- //
- //$k=$k_1+$k_2+$k_3+$k_4+$k_5+$k_6+$k_7+$k_8+$k_9+$k_1_0;
- //$q=$q_1+$q_2+$q_3+$q_4+$q_5+$q_6+$q_7+$q_8+$q_9+$q_1_0;
- //$m=$m_1+$m_2+$m_3+$m_4+$m_5+$m_6+$m_7+$m_8+$m_9+$m_1_0;
- //$n=$n_1+$n_2+$n_3+$n_4+$n_5+$n_6+$n_7+$n_8+$n_9+$n_1_0;
- //$o=$o_1+$o_2+$o_3+$o_4+$o_5+$o_6+$o_7+$o_8+$o_9+$o_1_0;
- //$p=$p_1+$p_2+$p_3+$p_4+$p_5+$p_6+$p_7+$p_8+$p_9+$p_1_0;
- //
- //}}}
+ session_start();
+ 
+
+ if (isset($_POST['k_9'])) {
+ if (!empty($_SESSION['post'])){
+ if (empty($_POST['k_9'])){ 
+ // Setting error for page 3.
+ $_SESSION['error_page5'] = "Mandatory field(s) are missing, Please fill it again";
+ header("location: holland-career-test-eng-5.php"); // Redirecting to third page.
+ } else {
+ foreach ($_POST as $key => $value) {
+ $_SESSION['post'][$key] = $value;
+ } 
+ extract($_SESSION['post']); // Function to extract array.
+ 
+ $k=$k_1+$k_2+$k_3+$k_4+$k_5+$k_6+$k_7+$k_8+$k_9+$k_1_0;
+ $q=$q_1+$q_2+$q_3+$q_4+$q_5+$q_6+$q_7+$q_8+$q_9+$q_1_0;
+ $m=$m_1+$m_2+$m_3+$m_4+$m_5+$m_6+$m_7+$m_8+$m_9+$m_1_0;
+ $n=$n_1+$n_2+$n_3+$n_4+$n_5+$n_6+$n_7+$n_8+$n_9+$n_1_0;
+ $o=$o_1+$o_2+$o_3+$o_4+$o_5+$o_6+$o_7+$o_8+$o_9+$o_1_0;
+ $p=$p_1+$p_2+$p_3+$p_4+$p_5+$p_6+$p_7+$p_8+$p_9+$p_1_0;
+
+ 
+ }}}else {
+  if (empty($_SESSION['error_page5'])) {
+  header("location: holland-career-test-eng-1.php");//redirecting to first page
+  }
+ }
+
+ $_SESSION['thinking'] = 5;
+ $_SESSION['building']   = 10;
  ?>
 
 <!--========================= Choose Language Section========================= -->
@@ -45,7 +56,8 @@
                 let's get started!
             </div>
             <div class="card-body">
-                <?php include('../components/holland-career-test/career-interest-profile.inc.php'); ?>
+                <?php 
+                 include('../components/holland-career-test/career-interest-profile.inc.php'); ?>
             </div>
         </div>
     </div>
