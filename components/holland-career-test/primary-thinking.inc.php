728b6eff217018b00cccbb2f6bb85b035c2fb9b4
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-10-07 15:15:47 +0530

    Add content for thinking

--- components/holland-career-test/primary-thinking.inc.php
+++ components/holland-career-test/primary-thinking.inc.php
@@ -1,37 +1,40 @@
 <h4 class="card-title mt-4">YOUR PRIMARY INTEREST AREA IS THINKING</h4>
-<p class="card-text"></p>
+<p class="card-text">As a Thinker, you enjoy working with ideas and theories. You want to solve complex problems through
+    rational, logical analysis. You are fascinated by the sciences and the process of discovering new information about
+    the world around you. You are drawn to environments where research is the focus, such as academia or scientific
+    industries. You want to explore and discover new ideas in your work.</p>
 
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-4 mb-3">
-            <span ><strong>Top Job Tasks</strong><span>
-            <ul class="list-group mt-1">
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-            </ul>
+            <span><strong>Top Job Tasks</strong><span>
+                    <ul class="list-group mt-1">
+                        <li class="list-item">Researching</li>
+                        <li class="list-item">Experimenting</li>
+                        <li class="list-item">Theorizing</li>
+                        <li class="list-item">Analyzing</li>
+                        <li class="list-item">Problem Solving</li>
+                    </ul>
         </div>
         <div class="col-md-4 mb-3">
-            <span ><strong>Your Core Values</strong><span>
-            <ul class="list-group mt-1">
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-            </ul>
+            <span><strong>Your Core Values</strong><span>
+                    <ul class="list-group mt-1">
+                        <li class="list-item">Discovery</li>
+                        <li class="list-item">Understanding</li>
+                        <li class="list-item">Logic</li>
+                        <li class="list-item">Independence</li>
+                        <li class="list-item">Intellect</li>
+                    </ul>
         </div>
         <div class="col-md-4 mb-3">
-            <span ><strong>Key Personality Traits</strong><span>
-            <ul class="list-group mt-1">
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-            </ul>
+            <span><strong>Key Personality Traits</strong><span>
+                    <ul class="list-group mt-1">
+                        <li class="list-item">Intellectual</li>
+                        <li class="list-item">Curious</li>
+                        <li class="list-item">Logical</li>
+                        <li class="list-item">Analytical</li>
+                        <li class="list-item">Scholarly</li>
+                    </ul>
         </div>
     </div>
 </div>
