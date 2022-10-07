1668141a5dc0f7f279bc0571056c7c1200eb9490
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-10-07 16:00:33 +0530

    Add content for persuading

--- components/holland-career-test/eng-primary-persuading.inc.php
+++ components/holland-career-test/eng-primary-persuading.inc.php
@@ -4,75 +4,93 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-4 mb-3">
-            <span ><strong>Top Job Tasks</strong><span>
-            <ul class="list-group mt-1">
-                <li class="list-item">Managing</li>
-                <li class="list-item">Deciding</li>
-                <li class="list-item">Strategizing</li>
-                <li class="list-item">Selling</li>
-                <li class="list-item">Motivating</li>
-            </ul>
+            <span><strong>Top Job Tasks</strong><span>
+                    <ul class="list-group mt-1">
+                        <li class="list-item">Managing</li>
+                        <li class="list-item">Deciding</li>
+                        <li class="list-item">Strategizing</li>
+                        <li class="list-item">Selling</li>
+                        <li class="list-item">Motivating</li>
+                    </ul>
         </div>
         <div class="col-md-4 mb-3">
-            <span ><strong>Your Core Values</strong><span>
-            <ul class="list-group mt-1">
-                <li class="list-item">Influence</li>
-                <li class="list-item">Leadership</li>
-                <li class="list-item">Risk-Taking</li>
-                <li class="list-item">Achievement</li>
-                <li class="list-item">Initiative</li>
-            </ul>
+            <span><strong>Your Core Values</strong><span>
+                    <ul class="list-group mt-1">
+                        <li class="list-item">Influence</li>
+                        <li class="list-item">Leadership</li>
+                        <li class="list-item">Risk-Taking</li>
+                        <li class="list-item">Achievement</li>
+                        <li class="list-item">Initiative</li>
+                    </ul>
         </div>
         <div class="col-md-4 mb-3">
-            <span ><strong>Key Personality Traits</strong><span>
-            <ul class="list-group mt-1">
-                <li class="list-item">Assertive</li>
-                <li class="list-item">Energetic</li>
-                <li class="list-item">Confident</li>
-                <li class="list-item">Ambitious</li>
-                <li class="list-item">Adventurous</li>
-            </ul>
+            <span><strong>Key Personality Traits</strong><span>
+                    <ul class="list-group mt-1">
+                        <li class="list-item">Assertive</li>
+                        <li class="list-item">Energetic</li>
+                        <li class="list-item">Confident</li>
+                        <li class="list-item">Ambitious</li>
+                        <li class="list-item">Adventurous</li>
+                    </ul>
         </div>
     </div>
 </div>
 
-<p class="card-text"></p>
+<p class="card-text">Because you are a Persuader, you will often gravitate to roles that allow you to sell other people
+    on products, projects, or initiatives. You will find a natural home in the business world, but will enjoy any role
+    where you can set a course of action and use your ingenuity and influence to achieve your goals. You may feel drawn
+    to entrepreneurship and enjoy the risk inherent in starting a new venture.</p>
 
-<p class="card-text"></p>
+<p class="card-text">Persuaders tend to be natural salespeople with a high energy level and enthusiasm for engaging with
+    others. They are typically good at developing rapport with others, although they may overwhelm some with their
+    aggressive drive. They are typically competitive, goal-oriented, and ambitious.</p>
 
-<p class="card-text"></p>
+<p class="card-text">Persuaders like their work best when they can chase exciting goals, promote new ideas, and close
+    important deals. As a Persuader, your primary career goal will be to find a job where you can take the lead to start
+    and carry out initiatives, act quickly and decisively to set a course, and use your charisma to influence others.
+</p>
 
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-4 mb-3">
-            <span ><strong>text</strong><span>
-            <ul class="list-group mt-1">
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
-            </ul>
+            <span><strong>Sample Jobs for Persuaders</strong><span>
+                    <ul class="list-group mt-1">
+                        <li class="list-item">Executive</li>
+                        <li class="list-item">Attorney</li>
+                        <li class="list-item">Public Relations Specialist</li>
+                        <li class="list-item">Financial Manager</li>
+                        <li class="list-item">Personal Financial Advisor</li>
+                        <li class="list-item">Buyer or Purchasing Agent</li>
+                        <li class="list-item">Real Estate Agent or Broker</li>
+                        <li class="list-item">Securities Broker</li>
+                        <li class="list-item">Health Services Manager</li>
+                    </ul>
         </div>
         <div class="col-md-4 mb-3">
-            <span ><strong>text</strong><span>
-            <ul class="list-group mt-1">
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
-            </ul>
+            <span><strong>Career Fields for Persuaders</strong><span>
+                    <ul class="list-group mt-1">
+                        <li class="list-item">Sales</li>
+                        <li class="list-item">Marketing</li>
+                        <li class="list-item">Entrepreneurship</li>
+                        <li class="list-item">Management</li>
+                        <li class="list-item">Public Relations</li>
+                        <li class="list-item">Legal</li>
+                        <li class="list-item">Politics</li>
+                        <li class="list-item">Real Estate</li>
+                    </ul>
         </div>
         <div class="col-md-4 mb-3">
-            <span ><strong>text</strong><span>
-            <ul class="list-group mt-1">
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
-            </ul>
+            <span><strong>Areas of Study for Persuaders</strong><span>
+                    <ul class="list-group mt-1">
+                        <li class="list-item">Business Administration</li>
+                        <li class="list-item">Marketing or Advertising</li>
+                        <li class="list-item">Hospitality</li>
+                        <li class="list-item">Real Estate</li>
+                        <li class="list-item">Political Science</li>
+                        <li class="list-item">Communications</li>
+                        <li class="list-item">Law</li>
+                        <li class="list-item">Organizational Psychology</li>
+                    </ul>
         </div>
     </div>
 </div>
