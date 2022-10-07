06df8aa7d2f52b975cb4353862cc39f7ad8ff612
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-10-07 15:26:37 +0530

    Add content for creating

--- components/holland-career-test/primary-creating.inc.php
+++ components/holland-career-test/primary-creating.inc.php
@@ -1,4 +1,4 @@
-<h4 class="card-title mt-4">YOUR PRIMARY INTEREST AREA IS THINKING</h4>
+<h4 class="card-title mt-4">YOUR PRIMARY INTEREST AREA IS CREATING</h4>
 <p class="card-text"></p>
 
 <div class="container-fluid">
@@ -6,31 +6,31 @@
         <div class="col-md-4 mb-3">
             <span ><strong>Top Job Tasks</strong><span>
             <ul class="list-group mt-1">
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
+                <li class="list-item">Creating Art</li>
+                <li class="list-item">Writing</li>
+                <li class="list-item">Interpreting</li>
+                <li class="list-item">Designing</li>
+                <li class="list-item">Expressing Ideas</li>
             </ul>
         </div>
         <div class="col-md-4 mb-3">
             <span ><strong>Your Core Values</strong><span>
             <ul class="list-group mt-1">
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
+                <li class="list-item">Individuality</li>
+                <li class="list-item">Creativity</li>
+                <li class="list-item">Originality</li>
+                <li class="list-item">Freedom</li>
+                <li class="list-item">Flexibility</li>
             </ul>
         </div>
         <div class="col-md-4 mb-3">
             <span ><strong>Key Personality Traits</strong><span>
             <ul class="list-group mt-1">
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
+                <li class="list-item">Independent</li>
+                <li class="list-item">Intuitive</li>
+                <li class="list-item">Sensitive</li>
+                <li class="list-item">Imaginative</li>
+                <li class="list-item">Spontaneous</li>
             </ul>
         </div>
     </div>
