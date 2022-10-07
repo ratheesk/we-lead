d4f0a86332be448bfc32b3df1aa9d11312cf306c
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-10-07 15:20:37 +0530

    Add content for persuading

--- components/holland-career-test/primary-persuading.inc.php
+++ components/holland-career-test/primary-persuading.inc.php
@@ -1,4 +1,4 @@
-<h4 class="card-title mt-4">YOUR PRIMARY INTEREST AREA IS THINKING</h4>
+<h4 class="card-title mt-4">YOUR PRIMARY INTEREST AREA IS PERSUADING</h4>
 <p class="card-text"></p>
 
 <div class="container-fluid">
@@ -6,31 +6,31 @@
         <div class="col-md-4 mb-3">
             <span ><strong>Top Job Tasks</strong><span>
             <ul class="list-group mt-1">
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
+                <li class="list-item">Managing</li>
+                <li class="list-item">Deciding</li>
+                <li class="list-item">Strategizing</li>
+                <li class="list-item">Selling</li>
+                <li class="list-item">Motivating</li>
             </ul>
         </div>
         <div class="col-md-4 mb-3">
             <span ><strong>Your Core Values</strong><span>
             <ul class="list-group mt-1">
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
+                <li class="list-item">Influence</li>
+                <li class="list-item">Leadership</li>
+                <li class="list-item">Risk-Taking</li>
+                <li class="list-item">Achievement</li>
+                <li class="list-item">Initiative</li>
             </ul>
         </div>
         <div class="col-md-4 mb-3">
             <span ><strong>Key Personality Traits</strong><span>
             <ul class="list-group mt-1">
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
+                <li class="list-item">Assertive</li>
+                <li class="list-item">Energetic</li>
+                <li class="list-item">Confident</li>
+                <li class="list-item">Ambitious</li>
+                <li class="list-item">Adventurous</li>
             </ul>
         </div>
     </div>
