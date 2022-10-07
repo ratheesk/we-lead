3c4cf7a049c5fc5969f5fb149315ed634ec100b0
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-10-07 15:07:49 +0530

    Add template for all pages

--- components/holland-career-test/primary-organising.inc.php
+++ components/holland-career-test/primary-organising.inc.php
@@ -1,1 +1,39 @@
+<h4 class="card-title mt-4">YOUR PRIMARY INTEREST AREA IS BUILDING</h4>
+<p class="card-text">As a Builder, you prefer physical work that uses your hands and body and gives you a tangible
+    result for your efforts. You prefer working with concrete objects, not abstract concepts. You may be drawn to work
+    with tools, machines, plants, or animals. You like to be outdoors and be physically active throughout the day.</p>
 
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-4 mb-3">
+            <span ><strong>Your Core Values</strong><span>
+            <ul class="list-group mt-1">
+                <li class="list-item">Hello</li>
+                <li class="list-item">Hello</li>
+                <li class="list-item">Hello</li>
+                <li class="list-item">Hello</li>
+                <li class="list-item">Hello</li>
+            </ul>
+        </div>
+        <div class="col-md-4 mb-3">
+            <span ><strong>Your Core Values</strong><span>
+            <ul class="list-group mt-1">
+                <li class="list-item">Hello</li>
+                <li class="list-item">Hello</li>
+                <li class="list-item">Hello</li>
+                <li class="list-item">Hello</li>
+                <li class="list-item">Hello</li>
+            </ul>
+        </div>
+        <div class="col-md-4 mb-3">
+            <span ><strong>Your Core Values</strong><span>
+            <ul class="list-group mt-1">
+                <li class="list-item">Hello</li>
+                <li class="list-item">Hello</li>
+                <li class="list-item">Hello</li>
+                <li class="list-item">Hello</li>
+                <li class="list-item">Hello</li>
+            </ul>
+        </div>
+    </div>
+</div>
