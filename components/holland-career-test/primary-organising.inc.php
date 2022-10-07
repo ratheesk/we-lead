84cee61cd1e5393730b9bc43aef9dacf0eb317d4
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-10-07 15:24:26 +0530

    Add content for organising

--- components/holland-career-test/primary-organising.inc.php
+++ components/holland-career-test/primary-organising.inc.php
@@ -1,4 +1,4 @@
-<h4 class="card-title mt-4">YOUR PRIMARY INTEREST AREA IS THINKING</h4>
+<h4 class="card-title mt-4">YOUR PRIMARY INTEREST AREA IS ORGANIZING</h4>
 <p class="card-text"></p>
 
 <div class="container-fluid">
@@ -6,31 +6,31 @@
         <div class="col-md-4 mb-3">
             <span ><strong>Top Job Tasks</strong><span>
             <ul class="list-group mt-1">
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
+                <li class="list-item">Filing</li>
+                <li class="list-item">Calculating</li>
+                <li class="list-item">Processing</li>
+                <li class="list-item">Systematizing</li>
+                <li class="list-item">Following Procedures</li>
             </ul>
         </div>
         <div class="col-md-4 mb-3">
             <span ><strong>Your Core Values</strong><span>
             <ul class="list-group mt-1">
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
+                <li class="list-item">Structure</li>
+                <li class="list-item">Order</li>
+                <li class="list-item">Clarity</li>
+                <li class="list-item">Precision</li>
+                <li class="list-item">Attention to Detail</li>
             </ul>
         </div>
         <div class="col-md-4 mb-3">
             <span ><strong>Key Personality Traits</strong><span>
             <ul class="list-group mt-1">
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
+                <li class="list-item">Orderly</li>
+                <li class="list-item">Precise</li>
+                <li class="list-item">Detail-Oriented</li>
+                <li class="list-item">Conservative</li>
+                <li class="list-item">Thorough</li>
             </ul>
         </div>
     </div>
