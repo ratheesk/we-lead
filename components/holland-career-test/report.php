933009b4dde78525fab5cb912aef33e1e75327d0
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-10-07 14:32:45 +0530

    Add report.php

---
+++ components/holland-career-test/report.php
@@ -0,0 +1,27 @@
+<!--========================= Report Section========================= -->
+<section>
+    <div class="container">
+        <div class="section-title text-center mt-4">
+            <span data-aos="fade-up">Results</span>
+            <div class="text-left">
+                <p data-aos="fade-up">Welcome to your career report! There's a lot of information here, so take your
+                    time and read through it at your own pace.</p>
+                <p data-aos="fade-up">Your report is split up into different sections, each of which addresses a
+                    different factor in your career search. You'll learn about your interests, your personality, and the
+                    jobs and work environments that suit you best. You'll get personalized advice and suggestions for
+                    exploring your career options and planning a successful job search.</p>
+                <p data-aos="fade-up">You can read your report straight through, or you can skip to the sections that
+                    interest you most.</p>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header">
+                let's get started!
+            </div>
+            <div class="card-body">
+                <?php include('career-interest-profile.inc.php'); ?>
+            </div>
+        </div>
+    </div>
+</section>
+<!--========================= End Report Section========================= -->
--- pages/holland-career-test-eng-6.php
+++ pages/holland-career-test-eng-6.php
@@ -63,33 +63,6 @@
 
  ?>
 
-<!--========================= Choose Language Section========================= -->
-<section>
-    <div class="container">
-        <div class="section-title text-center mt-4">
-            <span data-aos="fade-up">Results</span>
-            <div class="text-left">
-                <p data-aos="fade-up">Welcome to your career report! There's a lot of information here, so take your
-                    time and read through it at your own pace.</p>
-                <p data-aos="fade-up">Your report is split up into different sections, each of which addresses a
-                    different factor in your career search. You'll learn about your interests, your personality, and the
-                    jobs and work environments that suit you best. You'll get personalized advice and suggestions for
-                    exploring your career options and planning a successful job search.</p>
-                <p data-aos="fade-up">You can read your report straight through, or you can skip to the sections that
-                    interest you most.</p>
-            </div>
-        </div>
-        <div class="card">
-            <div class="card-header">
-                let's get started!
-            </div>
-            <div class="card-body">
-                <?php 
-                 include('../components/holland-career-test/career-interest-profile.inc.php'); ?>
-            </div>
-        </div>
-    </div>
-</section>
-<!--========================= End Choose Language Section========================= -->
+<?php include('../components/holland-career-test/report.php'); ?>
 
 <?php include('../components/footer.inc.php'); ?>
