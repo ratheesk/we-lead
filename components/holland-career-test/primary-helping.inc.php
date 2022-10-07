028992b07133db411a0729e1c8b16c2ca4c08db9
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-10-07 15:18:40 +0530

    Add content for helping

--- components/holland-career-test/primary-helping.inc.php
+++ components/holland-career-test/primary-helping.inc.php
@@ -1,37 +1,39 @@
-<h4 class="card-title mt-4">YOUR PRIMARY INTEREST AREA IS THINKING</h4>
-<p class="card-text"></p>
+<h4 class="card-title mt-4">YOUR PRIMARY INTEREST AREA IS HELPING</h4>
+<p class="card-text">As a Helper, you strive to dedicate your work life to serving, caring for, and inspiring others.
+    You are highly attuned to the needs of the people around you and gain satisfaction from attending to those needs.
+    You are driven by values in your work, and motivated by an aspiration to make the world a better place.</p>
 
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-4 mb-3">
-            <span ><strong>Top Job Tasks</strong><span>
-            <ul class="list-group mt-1">
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-            </ul>
+            <span><strong>Top Job Tasks</strong><span>
+                    <ul class="list-group mt-1">
+                        <li class="list-item">Counseling</li>
+                        <li class="list-item">Assisting</li>
+                        <li class="list-item">Advising</li>
+                        <li class="list-item">Teaching</li>
+                        <li class="list-item">Providing Service</li>
+                    </ul>
         </div>
         <div class="col-md-4 mb-3">
-            <span ><strong>Your Core Values</strong><span>
-            <ul class="list-group mt-1">
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-            </ul>
+            <span><strong>Your Core Values</strong><span>
+                    <ul class="list-group mt-1">
+                        <li class="list-item">Cooperation</li>
+                        <li class="list-item">Service</li>
+                        <li class="list-item">Altruism</li>
+                        <li class="list-item">Connection</li>
+                        <li class="list-item">Compassion</li>
+                    </ul>
         </div>
         <div class="col-md-4 mb-3">
-            <span ><strong>Key Personality Traits</strong><span>
-            <ul class="list-group mt-1">
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-                <li class="list-item">Hello</li>
-            </ul>
+            <span><strong>Key Personality Traits</strong><span>
+                    <ul class="list-group mt-1">
+                        <li class="list-item">Compassionate</li>
+                        <li class="list-item">Patient</li>
+                        <li class="list-item">Helpful</li>
+                        <li class="list-item">Friendly</li>
+                        <li class="list-item">Generous</li>
+                    </ul>
         </div>
     </div>
 </div>
