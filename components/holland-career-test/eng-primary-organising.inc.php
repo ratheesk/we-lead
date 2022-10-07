135f79f40e73f4e65848be2303b348fc82c1f2e8
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-10-07 16:05:02 +0530

    Add content for organising

--- components/holland-career-test/eng-primary-organising.inc.php
+++ components/holland-career-test/eng-primary-organising.inc.php
@@ -36,42 +36,54 @@
     </div>
 </div>
 
-<p class="card-text"></p>
+<p class="card-text">Because you are an Organizer, you are interested in keeping orderly systems in place. You will often enjoy office and administrative work, especially working with systems, records, and files. You like to maintain structure and enforce the rules, and want to work in a well organized and predictable environment.</p>
 
-<p class="card-text"></p>
+<p class="card-text">Organizers are detail-oriented and responsible people with an appreciation for structure and order. They typically take things literally and have little tolerance for sloppiness or people who try to bend the rules. They are cautious and careful and typically dislike taking risks.</p>
 
-<p class="card-text"></p>
+<p class="card-text">Organizers like their work best when they can carefully systematize processes to work toward well-defined goals. As an Organizer, your primary career goal will be to find a job where you can focus on the details and create reliable systems to produce  predictable, precise results.</p>
 
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-4 mb-3">
-            <span ><strong>text</strong><span>
+            <span ><strong>Sample Jobs for Organizers</strong><span>
             <ul class="list-group mt-1">
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
+                <li class="list-item">Accountant or Auditor</li>
+                <li class="list-item">Actuary</li>
+                <li class="list-item">Budget Analyst</li>
+                <li class="list-item">Paralegal</li>
+                <li class="list-item">Medical Transcriptionist</li>
+                <li class="list-item">Financial Analyst</li>
+                <li class="list-item">Administrative Assistant</li>
+                <li class="list-item">Cost Estimator</li>
+                <li class="list-item">Court Reporter</li>
             </ul>
         </div>
         <div class="col-md-4 mb-3">
-            <span ><strong>text</strong><span>
+            <span ><strong>Career Fields for Organizers</strong><span>
             <ul class="list-group mt-1">
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
+                <li class="list-item">Administration</li>
+                <li class="list-item">Banking and Finance</li>
+                <li class="list-item">Accounting</li>
+                <li class="list-item">Office Management</li>
+                <li class="list-item">Information Management</li>
+                <li class="list-item">Computers and Technology</li>
+                <li class="list-item">Engineering</li>
+                <li class="list-item">Law Enforcement</li>
+                <li class="list-item">Military</li>
             </ul>
         </div>
         <div class="col-md-4 mb-3">
-            <span ><strong>text</strong><span>
+            <span ><strong>Areas of Study for Organizers</strong><span>
             <ul class="list-group mt-1">
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
+                <li class="list-item">Business Administration</li>
+                <li class="list-item">Accounting or Actuarial Science</li>
+                <li class="list-item">Finance</li>
+                <li class="list-item">Law</li>
+                <li class="list-item">Criminal Justice</li>
+                <li class="list-item">Mathematics or Statistics</li>
+                <li class="list-item">Information Systems</li>
+                <li class="list-item">Engineering</li>
+                <li class="list-item">Computer Science</li>
             </ul>
         </div>
     </div>
