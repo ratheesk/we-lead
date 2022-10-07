71dc5f523c7f43b3b637cc512441af5ce3f38951
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-10-07 15:44:46 +0530

    Add content for building

--- components/holland-career-test/eng-primary-building.inc.php
+++ components/holland-career-test/eng-primary-building.inc.php
@@ -6,75 +6,93 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-4 mb-3">
-            <span ><strong>Top Job Tasks</strong><span>
-            <ul class="list-group mt-1">
-                <li class="list-item">Building</li>
-                <li class="list-item">Repairing</li>
-                <li class="list-item">Taking Action</li>
-                <li class="list-item">Using Machines</li>
-                <li class="list-item">Using Tools</li>
-            </ul>
+            <span><strong>Top Job Tasks</strong><span>
+                    <ul class="list-group mt-1">
+                        <li class="list-item">Building</li>
+                        <li class="list-item">Repairing</li>
+                        <li class="list-item">Taking Action</li>
+                        <li class="list-item">Using Machines</li>
+                        <li class="list-item">Using Tools</li>
+                    </ul>
         </div>
         <div class="col-md-4 mb-3">
-            <span ><strong>Your Core Values</strong><span>
-            <ul class="list-group mt-1">
-                <li class="list-item">Practicality</li>
-                <li class="list-item">Productivity</li>
-                <li class="list-item">Structure</li>
-                <li class="list-item">Independence</li>
-                <li class="list-item">Physical Skill</li>
-            </ul>
+            <span><strong>Your Core Values</strong><span>
+                    <ul class="list-group mt-1">
+                        <li class="list-item">Practicality</li>
+                        <li class="list-item">Productivity</li>
+                        <li class="list-item">Structure</li>
+                        <li class="list-item">Independence</li>
+                        <li class="list-item">Physical Skill</li>
+                    </ul>
         </div>
         <div class="col-md-4 mb-3">
-            <span ><strong>Key Personality Traits</strong><span>
-            <ul class="list-group mt-1">
-                <li class="list-item">Realistic</li>
-                <li class="list-item">Sensible</li>
-                <li class="list-item">Mechanical</li>
-                <li class="list-item">Traditional</li>
-                <li class="list-item">Down-to-earth</li>
-            </ul>
+            <span><strong>Key Personality Traits</strong><span>
+                    <ul class="list-group mt-1">
+                        <li class="list-item">Realistic</li>
+                        <li class="list-item">Sensible</li>
+                        <li class="list-item">Mechanical</li>
+                        <li class="list-item">Traditional</li>
+                        <li class="list-item">Down-to-earth</li>
+                    </ul>
         </div>
     </div>
 </div>
 
-<p class="card-text"></p>
+<p class="card-text">Because you are a Builder, you will prefer a career that allows you to use and hone your hands-on
+    skills. You enjoy learning to construct, craft, repair, maintain, and tinker. You like creating practical things and
+    fixing necessary systems. </p>
 
-<p class="card-text"></p>
+<p class="card-text">Builders tend to be pragmatic, down-to-earth people who take a “just do it” approach to life.
+    Builders often dislike having to explain or discuss their work with others, preferring instead to jump in and simply
+    get it done.</p>
 
-<p class="card-text"></p>
+<p class="card-text">Builders like their work best when they can see a real, physical result of their efforts. As a
+    Builder, your primary career goal will be to discover a job where you can use your physical or mechanical skills to
+    take useful, observable action on the world around you.</p>
 
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-4 mb-3">
-            <span ><strong>text</strong><span>
-            <ul class="list-group mt-1">
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
-            </ul>
+            <span><strong>Sample Jobs for Builders</strong><span>
+                    <ul class="list-group mt-1">
+                        <li class="list-item">Police Officer or Detective</li>
+                        <li class="list-item">Pilot</li>
+                        <li class="list-item">Electrician</li>
+                        <li class="list-item">Carpenter</li>
+                        <li class="list-item">Auto Mechanic</li>
+                        <li class="list-item">Building Inspector</li>
+                        <li class="list-item">Machinist</li>
+                        <li class="list-item">Civil Engineer</li>
+                        <li class="list-item">Athlete or Coach</li>
+                    </ul>
         </div>
         <div class="col-md-4 mb-3">
-            <span ><strong>text</strong><span>
-            <ul class="list-group mt-1">
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
-            </ul>
+            <span><strong>Career Fields for Builders</strong><span>
+                    <ul class="list-group mt-1">
+                        <li class="list-item">Construction</li>
+                        <li class="list-item">Mechanics</li>
+                        <li class="list-item">Transportation & Distribution</li>
+                        <li class="list-item">Military</li>
+                        <li class="list-item">Athletics</li>
+                        <li class="list-item">Farming or Ranching</li>
+                        <li class="list-item">Manufacturing and Industry</li>
+                        <li class="list-item">Maintenance and Repair</li>
+                        <li class="list-item">Food Preparation</li>
+                    </ul>
         </div>
         <div class="col-md-4 mb-3">
-            <span ><strong>text</strong><span>
-            <ul class="list-group mt-1">
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
-                <li class="list-item">text</li>
-            </ul>
+            <span><strong>text</strong><span>
+                    <ul class="list-group mt-1">
+                        <li class="list-item">Physical Education or Exercise Science</li>
+                        <li class="list-item">Construction Management</li>
+                        <li class="list-item">Engineering</li>
+                        <li class="list-item">Agriculture</li>
+                        <li class="list-item">Environmental Science</li>
+                        <li class="list-item">Food Science</li>
+                        <li class="list-item">Forestry</li>
+                        <li class="list-item">Construction, Maintenance or Repair</li>
+                        <li class="list-item">Culinary Arts</li>
+                    </ul>
         </div>
     </div>
 </div>
