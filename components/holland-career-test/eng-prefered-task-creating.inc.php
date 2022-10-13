7b468574b81ee0d2c0bbdf42e2ec8c872d75380e
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-10-13 16:00:13 +0530

    Add prefered tasks for creating

--- components/holland-career-test/eng-prefered-task-creating.inc.php
+++ components/holland-career-test/eng-prefered-task-creating.inc.php
@@ -1,8 +1,8 @@
 <ul class="list-group mt-1 ml-3">
-    <li class="list-item">Assertive</li>
-    <li class="list-item">Energetic</li>
-    <li class="list-item">Confident</li>
-    <li class="list-item">Ambitious</li>
-    <li class="list-item">Adventurous</li>
-    <li class="list-item">Adventurous</li>
+    <li class="list-item">Working with forms, colors, patterns, or other visual elements</li>
+    <li class="list-item">Working with aesthetic or expressive media like dance, music, or drama</li>
+    <li class="list-item">Creating a visually appealing presentation or design</li>
+    <li class="list-item">Coming up with an original idea with few or no guidelines to follow</li>
+    <li class="list-item">Working on what inspires you at the moment, rather than a predetermined task</li>
+    <li class="list-item">Being imaginative, creative, and original</li>
 </ul>
