568d7930761362f1580e8dae1afab133a15ad080
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-10-13 16:57:33 +0530

    Add template for questions

---
+++ components/holland-career-test/eng-questions-creating.inc.php
@@ -0,0 +1,6 @@
+<ul class="list-group mt-1 ml-3">
+    <li class="list-item">Use</li>
+    <li class="list-item">Work</li>
+    <li class="list-item">Follow</li>
+    <li class="list-item">Ensure</li>
+</ul>
