8e4bdb2a1ba0ec19b88e72a141324c049a7b7124
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-10-13 15:43:45 +0530

    Add prefered task for building

--- components/holland-career-test/eng-prefered-task-building.inc.php
+++ components/holland-career-test/eng-prefered-task-building.inc.php
@@ -1,8 +1,8 @@
 <ul class="list-group mt-1 ml-3">
-    <li class="list-item">Assertive</li>
-    <li class="list-item">Energetic</li>
-    <li class="list-item">Confident</li>
-    <li class="list-item">Ambitious</li>
-    <li class="list-item">Adventurous</li>
-    <li class="list-item">Adventurous</li>
+    <li class="list-item">Working with tools or machines</li>
+    <li class="list-item">Working with plants or animals</li>
+    <li class="list-item">Repairing or maintaining things, especially mechanical systems</li>
+    <li class="list-item">Building, constructing, or crafting things</li>
+    <li class="list-item">Being athletic or physical</li>
+    <li class="list-item">Making something tangible, practical and useful</li>
 </ul>
