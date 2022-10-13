43e27addf9acd41a1cb794930fc53cf109c7ab74
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-10-13 15:57:50 +0530

    Add preferd tasks for organising

--- components/holland-career-test/eng-prefered-task-organising.inc.php
+++ components/holland-career-test/eng-prefered-task-organising.inc.php
@@ -1,8 +1,8 @@
 <ul class="list-group mt-1 ml-3">
-    <li class="list-item">Assertive</li>
-    <li class="list-item">Energetic</li>
-    <li class="list-item">Confident</li>
-    <li class="list-item">Ambitious</li>
-    <li class="list-item">Adventurous</li>
-    <li class="list-item">Adventurous</li>
+    <li class="list-item">Managing data, files, or other forms of information</li>
+    <li class="list-item">Keeping things in order</li>
+    <li class="list-item">Developing organizational systems</li>
+    <li class="list-item">Following clear procedures</li>
+    <li class="list-item">Ensuring accuracy and correctness</li>
+    <li class="list-item">Maintaining efficient and well-ordered processes</li>
 </ul>
