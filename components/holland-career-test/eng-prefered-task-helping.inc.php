c6b12e2381b1c0ece39d4aefe72efaf2a6954fa9
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-10-13 15:59:00 +0530

    Add prefered tasks for helping

--- components/holland-career-test/eng-prefered-task-helping.inc.php
+++ components/holland-career-test/eng-prefered-task-helping.inc.php
@@ -1,8 +1,8 @@
 <ul class="list-group mt-1 ml-3">
-    <li class="list-item">Assertive</li>
-    <li class="list-item">Energetic</li>
-    <li class="list-item">Confident</li>
-    <li class="list-item">Ambitious</li>
-    <li class="list-item">Adventurous</li>
-    <li class="list-item">Adventurous</li>
+    <li class="list-item">Caring for others’ personal needs</li>
+    <li class="list-item">Educating or training people</li>
+    <li class="list-item">Counseling people on their personal issues</li>
+    <li class="list-item">Looking after vulnerable people, including the sick, very young, or very old</li>
+    <li class="list-item">Helping others grow and develop</li>
+    <li class="list-item">Being supportive and sympathetic</li>
 </ul>
