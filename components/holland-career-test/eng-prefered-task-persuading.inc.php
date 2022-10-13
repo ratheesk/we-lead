f5e57e52498d62c704fd27fbec4ccf4c2df8d056
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-10-13 15:56:33 +0530

    Add prefered task for persuading

--- components/holland-career-test/eng-prefered-task-persuading.inc.php
+++ components/holland-career-test/eng-prefered-task-persuading.inc.php
@@ -1,8 +1,8 @@
 <ul class="list-group mt-1 ml-3">
-    <li class="list-item">Assertive</li>
-    <li class="list-item">Energetic</li>
-    <li class="list-item">Confident</li>
-    <li class="list-item">Ambitious</li>
-    <li class="list-item">Adventurous</li>
-    <li class="list-item">Adventurous</li>
+    <li class="list-item">Selling products or services</li>
+    <li class="list-item">Leading or managing a team</li>
+    <li class="list-item">Pitching ideas or initiatives</li>
+    <li class="list-item">Starting a new business or other venture</li>
+    <li class="list-item">Speaking in front of groups of people</li>
+    <li class="list-item">Influencing people to your way of thinking</li>
 </ul>
