763b4ab53e4520cbc2d8eb7262f948a4d25a5aa2
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-10-25 13:17:20 +0530

    Show all primary interest areas

--- components/holland-career-test/eng-report.php
+++ components/holland-career-test/eng-report.php
@@ -78,28 +78,30 @@
 
                 <!--Primary interest area-->
                 <?php
-                switch ($primary_interest_area[1]) {
-                    case "build":
-                        include('eng-primary-building.inc.php');
-                        break;
-                    case "think":
-                        include('eng-primary-thinking.inc.php');
-                        break;
-                    case "creat":
-                        include('eng-primary-creating.inc.php');
-                        break;
-                    case "help":
-                        include('eng-primary-helping.inc.php');
-                        break;
-                    case "persuade":
-                        include('eng-primary-persuading.inc.php');
-                        break;
-                    case "organise":
-                        include('eng-primary-organising.inc.php');
-                        break;
-                    default:
-                        echo "Something went wrong";
-                }
+                foreach ($same_primary_interest_areas as $value) {
+                    switch ($value) {
+                        case "build":
+                            include('eng-primary-building.inc.php');
+                            break;
+                        case "think":
+                            include('eng-primary-thinking.inc.php');
+                            break;
+                        case "creat":
+                            include('eng-primary-creating.inc.php');
+                            break;
+                        case "help":
+                            include('eng-primary-helping.inc.php');
+                            break;
+                        case "persuade":
+                            include('eng-primary-persuading.inc.php');
+                            break;
+                        case "organise":
+                            include('eng-primary-organising.inc.php');
+                            break;
+                        default:
+                            echo "Something went wrong";
+                    }
+                 }
                 ?>
                 <!--End of Primary interest area-->
 
