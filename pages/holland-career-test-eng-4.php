4892e478aff25560fa01607739e5368fdd0e349f
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-10-29 23:29:07 +0530

    install mpdf & fix bug

--- pages/holland-career-test-eng-4.php
+++ pages/holland-career-test-eng-4.php
@@ -1,6 +1,3 @@
-<?php include('../components/header.inc.php'); ?>
-
-
 <?php
 session_start();
 // Checking first page values for empty,If it finds any blank field then redirected to first page.
@@ -22,6 +19,8 @@ if (isset($_POST['k_5'])){
  }
 }
 ?>
+
+<?php include('../components/header.inc.php'); ?>
 
  <!--========================= Choose Language Section========================= -->
 
@@ -83,13 +82,13 @@ if (!empty($_SESSION['error_page4'])) {
                     <div class="form-group">
                         <div class="form-check form-check-inline">
                             <label class="form-check-label" for="'.$data[$i][1].'_1">
-                                <input class="joyful" type="radio" name="'.$data[$i][1].'" id="'.$data[$i][1].'_1" value="1"  required="required">
+                                <input class="joyful" type="radio" name="'.$data[$i][1].'" id="'.$data[$i][1].'_1" value="5"  required="required">
                                 <svg viewBox="0 0 24 24"><path d="M12,17.5C14.33,17.5 16.3,16.04 17.11,14H6.89C7.69,16.04 9.67,17.5 12,17.5M8.5,11A1.5,1.5 0 0,0 10,9.5A1.5,1.5 0 0,0 8.5,8A1.5,1.5 0 0,0 7,9.5A1.5,1.5 0 0,0 8.5,11M15.5,11A1.5,1.5 0 0,0 17,9.5A1.5,1.5 0 0,0 15.5,8A1.5,1.5 0 0,0 14,9.5A1.5,1.5 0 0,0 15.5,11M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2C6.47,2 2,6.5 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" /></svg>
                             </label>
                         </div>
                         <div class="form-check form-check-inline">                            
                             <label class="form-check-label" for="'.$data[$i][1].'_2">
-                                <input class="happy" type="radio" name="'.$data[$i][1].'" id="'.$data[$i][1].'_2" value="2">
+                                <input class="happy" type="radio" name="'.$data[$i][1].'" id="'.$data[$i][1].'_2" value="4">
                                 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="100%" height="100%" viewBox="0 0 24 24"><path d="M20,12A8,8 0 0,0 12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20A8,8 0 0,0 20,12M22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2A10,10 0 0,1 22,12M10,9.5C10,10.3 9.3,11 8.5,11C7.7,11 7,10.3 7,9.5C7,8.7 7.7,8 8.5,8C9.3,8 10,8.7 10,9.5M17,9.5C17,10.3 16.3,11 15.5,11C14.7,11 14,10.3 14,9.5C14,8.7 14.7,8 15.5,8C16.3,8 17,8.7 17,9.5M12,17.23C10.25,17.23 8.71,16.5 7.81,15.42L9.23,14C9.68,14.72 10.75,15.23 12,15.23C13.25,15.23 14.32,14.72 14.77,14L16.19,15.42C15.29,16.5 13.75,17.23 12,17.23Z" /></svg>
                         </label>
                         </div>                        
@@ -101,13 +100,13 @@ if (!empty($_SESSION['error_page4'])) {
                         </div>
                         <div class="form-check form-check-inline">                            
                             <label class="form-check-label" for="'.$data[$i][1].'_4">
-                                <input class="bore" type="radio" name="'.$data[$i][1].'" id="'.$data[$i][1].'_4" value="4">
+                                <input class="bore" type="radio" name="'.$data[$i][1].'" id="'.$data[$i][1].'_4" value="2">
                                 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="100%" height="100%" viewBox="0 0 24 24"><path d="M20,12A8,8 0 0,0 12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20A8,8 0 0,0 20,12M22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2A10,10 0 0,1 22,12M15.5,8C16.3,8 17,8.7 17,9.5C17,10.3 16.3,11 15.5,11C14.7,11 14,10.3 14,9.5C14,8.7 14.7,8 15.5,8M10,9.5C10,10.3 9.3,11 8.5,11C7.7,11 7,10.3 7,9.5C7,8.7 7.7,8 8.5,8C9.3,8 10,8.7 10,9.5M12,14C13.75,14 15.29,14.72 16.19,15.81L14.77,17.23C14.32,16.5 13.25,16 12,16C10.75,16 9.68,16.5 9.23,17.23L7.81,15.81C8.71,14.72 10.25,14 12,14Z" /></svg>
                             </label>
                         </div>
                         <div class="form-check form-check-inline">
                             <label class="form-check-label" for="'.$data[$i][1].'_5">
-                                <input class="dull" type="radio" name="'.$data[$i][1].'" id="'.$data[$i][1].'_5" value="5">
+                                <input class="dull" type="radio" name="'.$data[$i][1].'" id="'.$data[$i][1].'_5" value="1">
                                 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="100%" height="100%" viewBox="0 0 24 24"><path d="M20,12A8,8 0 0,0 12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20A8,8 0 0,0 20,12M22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2A10,10 0 0,1 22,12M15.5,8C16.3,8 17,8.7 17,9.5C17,10.3 16.3,11 15.5,11C14.7,11 14,10.3 14,9.5C14,8.7 14.7,8 15.5,8M10,9.5C10,10.3 9.3,11 8.5,11C7.7,11 7,10.3 7,9.5C7,8.7 7.7,8 8.5,8C9.3,8 10,8.7 10,9.5M12,14C9.67,14 7.69,15.46 6.89,17.5H17.11C16.31,15.46 14.33,14 12,14Z" /></svg>
                             </label>
                         </div>
