f9bd6a605cb0c476fabd0bc8124c842ed7e8fd14
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-10-29 16:34:50 +0530

    Add carousel

--- pages/index.php
+++ pages/index.php
@@ -1,3 +1,4 @@
 <?php include('../components/header.inc.php'); ?>
+<?php include('../components/carousel.inc.php'); ?>
 <?php include('../components/holland-career-test/choose-lang.inc.php'); ?>
 <?php include('../components/footer.inc.php'); ?>
