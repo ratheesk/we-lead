0ced710d02bb30331be3efb8b88720865b7c2f7a
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-11-02 18:13:17 +0530

    Add validation for report

--- pages/holland-code-career-test-report.php
+++ pages/holland-code-career-test-report.php
@@ -4,16 +4,16 @@
  session_start();
  
 
- if (!empty($_SESSION['post'])){
+ if (!empty($_SESSION['post']) && !empty($_SESSION['language'])){
  
- extract($_SESSION['post']); // Function to extract array.
- 
- $k=$k_1+$k_2+$k_3+$k_4+$k_5+$k_6+$k_7+$k_8+$k_9+$k_1_0-10;
- $q=$q_1+$q_2+$q_3+$q_4+$q_5+$q_6+$q_7+$q_8+$q_9+$q_1_0-10;
- $m=$m_1+$m_2+$m_3+$m_4+$m_5+$m_6+$m_7+$m_8+$m_9+$m_1_0-10;
- $n=$n_1+$n_2+$n_3+$n_4+$n_5+$n_6+$n_7+$n_8+$n_9+$n_1_0-10;
- $o=$o_1+$o_2+$o_3+$o_4+$o_5+$o_6+$o_7+$o_8+$o_9+$o_1_0-10;
- $p=$p_1+$p_2+$p_3+$p_4+$p_5+$p_6+$p_7+$p_8+$p_9+$p_1_0-10;
+    extract($_SESSION['post']); // Function to extract array.
+    
+    $k=$k_1+$k_2+$k_3+$k_4+$k_5+$k_6+$k_7+$k_8+$k_9+$k_1_0-10;
+    $q=$q_1+$q_2+$q_3+$q_4+$q_5+$q_6+$q_7+$q_8+$q_9+$q_1_0-10;
+    $m=$m_1+$m_2+$m_3+$m_4+$m_5+$m_6+$m_7+$m_8+$m_9+$m_1_0-10;
+    $n=$n_1+$n_2+$n_3+$n_4+$n_5+$n_6+$n_7+$n_8+$n_9+$n_1_0-10;
+    $o=$o_1+$o_2+$o_3+$o_4+$o_5+$o_6+$o_7+$o_8+$o_9+$o_1_0-10;
+    $p=$p_1+$p_2+$p_3+$p_4+$p_5+$p_6+$p_7+$p_8+$p_9+$p_1_0-10;
 
      //function to get the primary interest area
      function primary_interest($args) {
@@ -95,7 +95,7 @@
     $same_least_interest_areas = same_max_value($obatained_values,$least_interest_area[1],$least_interest_area[0]);
 
  } else {
-    if (empty($_SESSION['error_page6'])) {
+    if (empty($_SESSION['error'])) {
     header("location: holland-code-career-test.php");//redirecting to first page
     }
    }
