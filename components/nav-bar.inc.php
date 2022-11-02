97b41d2e6bdadae0af3d80bab02278012b727d5d
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-11-02 23:08:19 +0530

    Cleanup

--- components/nav-bar.inc.php
+++ components/nav-bar.inc.php
@@ -7,35 +7,23 @@
         </button>
         <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
             <div class="navbar-nav me-auto p-3 p-lg-0">
-                <a href="index.html" class="nav-item nav-link active">Home</a>
-                <a href="about.html" class="nav-item nav-link">About Us</a>
-                <a href="service.html" class="nav-item nav-link">Services</a>
-                <a href="project.html" class="nav-item nav-link">Projects</a>
+                <a href="index.php" class="nav-item nav-link active">Home</a>
+                <a href="" class="nav-item nav-link">About Us</a>
+                <a href="" class="nav-item nav-link">Services</a>
+                <a href="" class="nav-item nav-link">Projects</a>
                 <div class="nav-item dropdown">
                     <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
                     <div class="dropdown-menu border-0 rounded-0 rounded-bottom m-0">
-                        <a href="feature.html" class="dropdown-item">Features</a>
-                        <a href="team.html" class="dropdown-item">Our Team</a>
-                        <a href="testimonial.html" class="dropdown-item">Testimonial</a>
-                        <a href="404.html" class="dropdown-item">404 Page</a>
+                        <a href="" class="dropdown-item">Features</a>
+                        <a href="" class="dropdown-item">Our Team</a>
+                        <a href="" class="dropdown-item">Testimonial</a>
+                        <a href="" class="dropdown-item">404 Page</a>
                     </div>
                 </div>
                 <a href="contact.html" class="nav-item nav-link">Contact Us</a>
             </div>
-            <a href="#" class="btn btn-sm btn-light rounded-pill py-2 px-4 d-none d-lg-block text-end">Get Started</a>
+            <a href="holland-code-career-test.php" class="btn btn-sm btn-light rounded-pill py-2 px-4 d-none d-lg-block text-end">Career Test</a>
         </div>
     </nav>
     <!-- Navbar End -->
 
-<!-- ======= Header ======= 
-<header id="header" class="fixed-top ">
-  <div class="container-fluid d-flex align-items-center justify-content-between">
-  <a class="navbar-brand" href="https://www.pdn.ac.lk/cgu/">
-    <img src="../assets/img/logo/University_of_Peradeniya_crest.png" width="40" height="40" class="d-inline-block align-top" alt="">
-    Career Guidance Unit
-  </a>
-
-    <a href="https://www.pdn.ac.lk/cgu/" class="get-started-btn scrollto">Find more</a>
-
-  </div>
-</header>End Header -->
