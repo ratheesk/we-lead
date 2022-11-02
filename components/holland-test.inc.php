825156f17d7830e532a61a817d165ab9a8614b42
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-11-02 23:06:06 +0530

    Add content

--- components/holland-test.inc.php
+++ components/holland-test.inc.php
@@ -1,27 +1,26 @@
 <div class="container-xxl py-5">
-        <div class="container">
-            <div class="row g-5">
-                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
-                    <div class="img-border">
-                        <img class="img-fluid" src="../assets/img/about.jpg" alt="">
-                    </div>
+    <div class="container">
+        <div class="row g-5">
+            <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="img-border">
+                    <img class="img-fluid" src="../assets/img/about.jpg" alt="">
                 </div>
-                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.5s">
-                    <div class="h-100">
-                        <h3 class="section-title bg-white text-start text-primary pe-3">Holland Code Career Test</h3>
-                        <h1 class="display-6 mb-4">#1 Digital Solution With <span class="text-primary">10 Years</span> Of Experience</h1>
-                        <p>Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
-                        <p class="mb-4">Stet no et lorem dolor et diam, amet duo ut dolore vero eos. No stet est diam rebum amet diam ipsum. Clita clita labore, dolor duo nonumy clita sit at, sed sit sanctus dolor eos.</p>
-                        <div class="d-flex align-items-center mb-4 pb-2">
-                            <img class="flex-shrink-0 rounded-circle" src="img/team-1.jpg" alt="" style="width: 50px; height: 50px;">
-                            <div class="ps-4">
-                                <h6>Jhon Doe</h6>
-                                <small>SEO & Founder</small>
-                            </div>
-                        </div>
-                        <a class="btn btn-primary rounded-pill py-3 px-5" href="">Read More</a>
-                    </div>
+            </div>
+            <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.5s">
+                <div class="h-100">
+                    <h3 class="section-title bg-white text-start text-primary pe-3">Holland Code Career Test</h3>
+
+                    <p>Take this <strong>FREE </strong>career test to discover your interest in different types of
+                        careers in the world and find your most suitable job catergaries to work.</p>
+                    <p>Be yourself and answer honestly, This is a great chance to discover the tasks to do and avoid
+                        with your next steps towards the career. This free career quiz uses the scientific Holland Code
+                        model to show you which jobs will suit your interests, talents, and aptitude.</p>
+                    <p class="mb-4">In this model there are six career interest areas such as Building, Thinking,
+                        Helping, Creating, Persuading and Organising. At the end of the test you can go through your
+                        report and download the report in PDF format if you wish.</p>
+                    <a class="btn btn-primary rounded-pill py-3 px-5" href="holland-code-career-test.php">Take Now</a>
                 </div>
             </div>
         </div>
     </div>
+</div>
