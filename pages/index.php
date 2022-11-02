6f2bf74f48eb5b2d2cbfbd87bdbf0e325f400768
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-11-02 22:45:23 +0530

    Create holland-test component

--- pages/index.php
+++ pages/index.php
@@ -3,5 +3,5 @@ session_start(); // Session starts here.
 ?>
 <?php include('../components/header.inc.php'); ?>
 <?php include('../components/carousel.inc.php'); ?>
-<?php include('../components/holland-code-career-test/choose-lang.inc.php'); ?>
+<?php include('../components/holland-test.inc.php'); ?>
 <?php include('../components/footer.inc.php'); ?>
