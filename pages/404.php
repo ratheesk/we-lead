c9ae6c650f84d57763adc62c4dc23040e9876d9d
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-11-18 16:54:19 +0530

    Cleanup

--- pages/404.php
+++ pages/404.php
@@ -1,6 +1,8 @@
 <?php
 $title = "We Lead | Page Not Found";
 $description = "Error 404";
-include('../components/header.inc.php'); ?>
-<?php include('../components/404.inc.php'); ?>
-<?php include('../components/footer.inc.php'); ?>
+
+include('../components/header.inc.php');
+include('../components/404.inc.php');
+include('../components/footer.inc.php');
+ ?>
