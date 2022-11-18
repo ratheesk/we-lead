3e2c3cd10b5e1bd0c42f95f69747c97d4df94df4
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-11-18 16:30:57 +0530

    Clean up

--- pages/index.php
+++ pages/index.php
@@ -1,9 +1,12 @@
 <?php
 session_start(); // Session starts here.
+
 $title = "We Lead";
 $description = "“We Lead” (Career Skills Society) is an initiative of the Career Guidance Unit of the University of Peradeniya. The ultimate goal of society is to guide career planning and develop the employability skills of Peradeniya graduates to reach their dream destinations in their professions.";
+
+include('../components/header.inc.php');
+include('../components/carousel.inc.php');
+include('../components/holland-test.inc.php');
+include('../components/footer.inc.php');
 ?>
-<?php include('../components/header.inc.php'); ?>
-<?php include('../components/carousel.inc.php'); ?>
-<?php include('../components/holland-test.inc.php'); ?>
-<?php include('../components/footer.inc.php'); ?>
+
