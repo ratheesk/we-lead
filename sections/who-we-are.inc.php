b420df9287b677445c9e161088e762f96f91d95c
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-11-24 15:01:25 +0530

    Add who we are section

---
+++ sections/who-we-are.inc.php
@@ -0,0 +1,9 @@
+    <!-- Who we are Start -->
+    <div class="container-xxl py-2">
+        <div class="container">
+            <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+                <h3 class="section-title text-center text-primary px-3">WHO WE ARE</h3>
+            </div>
+        </div>
+    </div>
+    <!-- Who we are End -->
