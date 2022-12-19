cf31eb276723e1fb15baafe4abfdf3d64a4c5e96
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-12-19 13:54:20 +0530

    Creatw who we are section

--- sections/about.inc.php
+++ sections/about.inc.php
@@ -4,42 +4,24 @@
             <div class="row g-5">
                 <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
                     <div class="h-100">
-                    <h3 class="section-title text-start text-primary pe-3">ABOUT US</h3>
-                        <p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
-                        <div class="row g-4">
-                            <div class="col-12">
-                                <div class="skill">
-                                    <div class="d-flex justify-content-between">
-                                        <p class="mb-2">Digital Marketing</p>
-                                        <p class="mb-2">85%</p>
-                                    </div>
-                                    <div class="progress">
-                                        <div class="progress-bar bg-primary" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-12">
-                                <div class="skill">
-                                    <div class="d-flex justify-content-between">
-                                        <p class="mb-2">SEO & Backlinks</p>
-                                        <p class="mb-2">90%</p>
-                                    </div>
-                                    <div class="progress">
-                                        <div class="progress-bar bg-primary" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-12">
-                                <div class="skill">
-                                    <div class="d-flex justify-content-between">
-                                        <p class="mb-2">Design & Development</p>
-                                        <p class="mb-2">95%</p>
-                                    </div>
-                                    <div class="progress">
-                                        <div class="progress-bar bg-primary" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100"></div>
-                                    </div>
-                                </div>
-                            </div>
+                        <h3 class="section-title-white text-start text-primary pe-3">OBJECTIVES</h3>
+                        <p class="mb-4">These are our objectives to empower undergraduates to become professionals by
+                            creating and fostering an ecosystem to enhance their career skills and related competencies,
+                            jointly with industrial and international corporations.</p>
+                        <div class="row">
+                            <ul>
+                                <li>Develop career skills and other required competencies of undergraduates for enhanced
+                                    employability and creating a professional lifestyle.</li>
+                                <li>Develop students socioemotional skills.</li>
+                                <li>Improve leadership skills of a wider group of students through the administrative
+                                    activities of the society.</li>
+                                <li>Provide industrial and international exposure to enhance undergraduates’ workplace
+                                    skills.</li>
+                                <li>Assist developing a compatible social lifestyle to reach one’s full potential as
+                                    professionals.</li>
+                                <li>Provide opportunities for undergraduates to engage with the community and develop
+                                    their working skills.</li>
+                            </ul>
                         </div>
                     </div>
                 </div>
--- sections/who-we-are.inc.php
+++ sections/who-we-are.inc.php
@@ -1,8 +1,47 @@
     <!-- Who we are Start -->
-    <div class="container-xxl py-2">
+    <div class="container-xxl py-2 ">
         <div class="container">
-            <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+            <div class="text-center mx-auto mb-3 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
                 <h3 class="section-title text-center text-primary px-3">WHO WE ARE</h3>
+            </div>
+            <div class="row justify-content-center">
+            <div class="col-12 text-center w-75 wow fadeInUp ">
+                <p class="mb-4">“We Lead” (Career Skills Society) is an initiative of the Career Guidance Unit of the
+                    University of Peradeniya. The ultimate goal of society is to guide career planning and develop the
+                    employability skills of Peradeniya graduates to reach their dream destinations in their professions.
+                    We are providing our service to undergraduates and graduates. In addition, customized projects will
+                    be offered to the local community as university-to-community (U2C) services.</p>
+            </div>
+            <div class="row mb-4">
+                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="fact-item bg-light rounded text-center h-100 p-5">
+                        <i class="fa fa-certificate fa-4x text-primary mb-4"></i>
+                        <h5 class="mb-3">Years Experience</h5>
+                        <h1 class="display-5 mb-0" data-toggle="counter-up">20</h1>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+                    <div class="fact-item bg-light rounded text-center h-100 p-5">
+                        <i class="fa fa-users-cog fa-4x text-primary mb-4"></i>
+                        <h5 class="mb-3">Key Service Areas</h5>
+                        <h1 class="display-5 mb-0" data-toggle="counter-up">8</h1>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+                    <div class="fact-item bg-light rounded text-center h-100 p-5">
+                        <i class="fa fa-users fa-4x text-primary mb-4"></i>
+                        <h5 class="mb-3">Trained Graduates</h5>
+                        <h1 class="display-5 mb-0" data-toggle="counter-up">60000</h1>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.7s">
+                    <div class="fact-item bg-light rounded text-center h-100 p-5">
+                        <i class="fa fa-map-signs fa-4x text-primary mb-4"></i>
+                        <h5 class="mb-3">Workshops Per Year</h5>
+                        <h1 class="display-5 mb-0" data-toggle="counter-up">100</h1>
+                    </div>
+                </div>
+            </div>
             </div>
         </div>
     </div>
